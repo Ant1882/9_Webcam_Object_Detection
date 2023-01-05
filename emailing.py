41bb2f3e0b4d59306fb00265c3c1614a6100b20a
Ant1882 <antmarshall1882@gmail.com>
2023-01-05 14:07:12 +0000

    Use threading to avoid video stutter

--- emailing.py
+++ emailing.py
@@ -8,6 +8,7 @@ SENDER = "info@awembedded.com"
 RECEIVER = "info@awembedded.com"
 
 def send_email(image_path):
+    print("send_email function started")
     email_message = EmailMessage()
     email_message["Subject"] = "New customer showed up!"
     email_message.set_content("Hey, we just saw a new customer!")
@@ -22,6 +23,7 @@ def send_email(image_path):
     gmail.login(SENDER, PASSWORD)
     gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
     gmail.quit()
+    print("send_email function ended")
 
 
 if __name__ == "__main__":
